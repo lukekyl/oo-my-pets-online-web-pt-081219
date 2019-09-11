8dc9cac8a90115e13ed9d0b09da223dfc5531716
Kyle Luke <th.oldschool@gmail.com>
2019-09-11 02:51:49 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -7,7 +7,7 @@ class Cat
     self.owner_name=(owner)
   end
   def owner_name=(name)
-    owner = Owner.find_or_create_by_name(name)
+    self.owner = Owner.find_or_create_by_name(name)
     owner.buy_cat(self)
     #artist.pets
   end
