3421f68c1e2195c0b452d1e2efbc7e0ddc45ec7e
Kyle Luke <th.oldschool@gmail.com>
2019-09-11 02:12:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,18 @@
 class Owner
   attr_accessor
   attr_reader :name, :species
+  @@all = []
   
   def initialize(name)
     @name = name
     @species = "human"
+    save
+  end
+  def save
+    @@all << self
+  end
+  def self.all 
+    @@all 
   end
   def say_species
     p "I am a #{@species}."
