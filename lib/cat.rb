d5dbb99738d787860d4b4877c0d3fd56a0722005
Kyle Luke <th.oldschool@gmail.com>
2019-09-11 02:36:52 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -6,8 +6,8 @@ class Cat
     @name = name 
     @owner = self.owner(owner)
   end
-  def owner=(name)
-    self.owner = Owner.find_or_create_by_name(name)
+  def self.owner=(name)
+    owner = Owner.find_or_create_by_name(name)
     #owner.add_cat(self)
     #artist.pets
   end
